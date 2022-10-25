1379c641f7baae5ed758b62b7f96ee13e4c5af20
milind <milind.saurkar@gmail.com>
2022-10-25 20:14:32 +0530

    new

---
+++ Id.java
@@ -0,0 +1,10 @@
+class Student
+{
+  public static void main(String args[])
+  {
+   System.out.println("Name:Ram ");
+   System.out.println("College Name: ABC ");
+   System.out.println("Blood Group: AB");
+   System.out.println("Mobile no.: 123456789");
+  }
+}
